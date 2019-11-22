16da04abdf4a9aea4e990cc81f29e9b9519fa489
JACK_E <jshou403@gmail.com>
2019-11-22 17:18:05 -0500

    Math to determine the closest friend match to user works.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,16 +5,60 @@ module.exports = function (app) {
 
     // displays friends.js file in json format
     app.get("/api/friends", function (req, res) {
-        console.log(res);
+
+        // console.log(res);
         return res.json(friends);
     });
 
     app.post("/api/friends", function (req, res) {
 
+        // stores request from the get route into var
         var newFriend = req.body;
-        console.log(newFriend);
-        res.json(newFriend);
+        // logs var to terminal 
+        // console.log(newFriend);
+
+        var closestScore = 50;
+        var closestMatch = 0;
+
+        // math to compare newFriend var with existing friendsArr
+
+        // first loop through friendsArr
+        for (var i = 0; i < friends.length; i++) {
+
+            var diff = 0;
+
+            console.log("\nfriend #" + i + "\nfriend.AnswersArr = " + friends[i].answersArr);
+            console.log("newFriend" + newFriend.answersArr);
+
+            // then loop through each index of the newFriend.answersArr
+            for (var j = 0; j < newFriend.answersArr.length; j++) {
+
+                // take the absolute value of the difference of each index from friend[i] and newFriend
+                diff += Math.abs(newFriend.answersArr[j] - friends[i].answersArr[j]);
+
+                // console.log("\nnewFriend.answersArr[j] - " + newFriend.answersArr[j]);
+                // console.log("friends[i].answersArr[j] - " + friends[i].answersArr[j]);
+                // console.log("diff = " + diff + "\n"); 
+            }
+
+            if (diff < closestScore) {
+                closestScore = diff;
+                closestMatch = i;
+            }
+
+        }
+
+        console.log("friends[closestMatch] = " + i);
+        console.log(friends[closestMatch]);
+        console.log("closestScore = "  + closestScore);
+
+        // IS THIS REQUIRED?
+        // res.json(newFriend);
+
+        // pushes to friendsArr
         friends.push(newFriend);
+
+        // POST BEST FRIEND TO MODAL
 
         // console.log(newFriend);
 
