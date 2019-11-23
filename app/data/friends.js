5a6b4c9fe318c41bc82a13eaa60eea80fb4af1da
JACK_E <jshou403@gmail.com>
2019-11-22 23:37:38 -0500

    Update survey questions and friends array to new characters.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,54 +1,34 @@
 var friendsArr = [
     {
-        name: "Dale Cooper",
-        URL: "dale.jpg",
-        answersArr: [1,1,1,1,1,1,1,1,1,1]
+        name: "Homer",
+        URL: "/public/images/homer.jpg",
+        answersArr: [1,4,5,1,3,4,4,2,4,5]
     },
     {
-        name: "Nadine Hurley",
-        URL: "nadine.jpg",
-        answersArr: [2,2,2,2,2,2,2,2,2,2]
+        name: "Marge",
+        URL: "/public/images/marge.jpg",
+        answersArr: [4,5,3,5,5,5,5,5,1,1]
     },
     {
-        name: "James Hurley",
-        URL: "james.jpg",
-        answersArr: [3,3,3,3,3,3,3,3,3,3]
+        name: "Bart",
+        URL: "/public/images/bart.png",
+        answersArr: [5,1,5,2,1,1,1,1,5,5]
     },
     {
-        name: "Lucy Moran",
-        URL: "lucy.jpg",
-        answersArr: [4,4,4,4,4,4,4,4,4,4]
+        name: "Lisa",
+        URL: "/public/images/lisa.png",
+        answersArr: [5,4,3,5,3,5,5,5,1,1]
     },
     {
-        name: "Sheriff Truman",
-        URL: "truman.jpg",
-        answersArr: [5,5,5,5,5,5,5,5,5,5]
+        name: "Ned",
+        URL: "/public/images/ned.jpg",
+        answersArr: [3,5,4,5,3,5,5,3,1,3]
     },
     {
-        name: "Audrey Horne",
-        URL: "audrey.jpg",
-        answersArr: [1,1,1,1,1,2,2,2,2,2]
+        name: "Mr. Burns",
+        URL: "/public/images/monte.jpg",
+        answersArr: [2,1,4,2,5,1,1,1,2,2]
     },
-    {
-        name: "Dr. Jacoby",
-        URL: "drjacoby.jpg",
-        answersArr: [2,2,2,2,2,3,3,3,3,3]
-    },
-    {
-        name: "Ben Horne",
-        URL: "benhorne.jpg",
-        answersArr: [3,3,3,3,3,4,4,4,4,4]
-    },
-    {
-        name: "Josie Packard",
-        URL: "josie.jpg",
-        answersArr: [4,4,4,4,4,5,5,5,5,5]
-    },
-    {
-        name: "Leo Johnson",
-        URL: "leo.jpg",
-        answersArr: [5,5,5,5,5,1,1,1,1,1]
-    }
 ]
 
 module.exports = friendsArr;
