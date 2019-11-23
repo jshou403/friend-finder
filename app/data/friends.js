1c7723eea98b6dd12a75eea16db4049d39a810f9
JACK_E <jshou403@gmail.com>
2019-11-22 23:53:09 -0500

    Fixed image URLs.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 var friendsArr = [
     {
         name: "Homer",
-        URL: "/public/images/homer.jpg",
+        URL: "https://raw.githubusercontent.com/jshou403/friend-finder/master/app/public/images/homer.jpg",
         answersArr: [1,4,5,1,3,4,4,2,4,5]
     },
     {
         name: "Marge",
-        URL: "/public/images/marge.jpg",
+        URL: "https://raw.githubusercontent.com/jshou403/friend-finder/master/app/public/images/marge.jpg",
         answersArr: [4,5,3,5,5,5,5,5,1,1]
     },
     {
         name: "Bart",
-        URL: "/public/images/bart.png",
+        URL: "https://raw.githubusercontent.com/jshou403/friend-finder/master/app/public/images/bart.png",
         answersArr: [5,1,5,2,1,1,1,1,5,5]
     },
     {
         name: "Lisa",
-        URL: "/public/images/lisa.png",
+        URL: "https://raw.githubusercontent.com/jshou403/friend-finder/master/app/public/images/lisa.png",
         answersArr: [5,4,3,5,3,5,5,5,1,1]
     },
     {
         name: "Ned",
-        URL: "/public/images/ned.jpg",
+        URL: "https://raw.githubusercontent.com/jshou403/friend-finder/master/app/public/images/ned.jpg",
         answersArr: [3,5,4,5,3,5,5,3,1,3]
     },
     {
         name: "Mr. Burns",
-        URL: "/public/images/monte.jpg",
+        URL: "https://raw.githubusercontent.com/jshou403/friend-finder/master/app/public/images/monte.jpg",
         answersArr: [2,1,4,2,5,1,1,1,2,2]
     },
 ]
